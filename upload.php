e234439967374ce63ddf155812ff7f763a6cecc1
jahjuno <aina.apsa18@gmail.com>
2019-09-18 00:29:46 +0300

    Upload

---
+++ upload.php
@@ -0,0 +1,45 @@
+<?php
+$oFileInfos = $_FILES["fichier_image"];
+                $pdp_name= $oFileInfos["name"];
+                $pdp_name = str_replace(' ', '_', $pdp_name);
+                $pdp_temporaire = $oFileInfos["tmp_name"];
+                $code_erreur = $oFileInfos["error"];
+                $destination = "/var/www/html/stockage/$pdp_name";
+
+
+                switch($code_erreur)
+    {
+        case UPLOAD_ERR_OK:
+            if(copy($pdp_temporaire, $destination)){
+                $verify_insertion = $query_bdd->insertion_fichier_pdp($id, $pdp_name, $action);
+            }
+            else{
+                throw new Exception("Erreur copie fichier");
+            }
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new Exception("Aucun fichier séléctionner");
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+            throw new Exception("Taille fichier > upload_max_filesize");
+            break;
+        case UPLOAD_ERR_PARTIAL:
+            throw new Exception("Fichier partiellement transféré");
+            break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+            throw new Exception("Aucun répertoire temporaire");
+            break;
+        case UPLOAD_ERR_CANT_WRITE:
+            throw new Exception("Erreur lors de l’écriture du fichier sur disque");
+            break;
+        default:
+            throw new Exception("Fichier non transféré");
+            break;
+    }
+    if($verify_insertion === false){
+        throw new Exception("Erreur insertion fichier");
+    }
+    else{
+        echo "Succed";
+    }
+?>
