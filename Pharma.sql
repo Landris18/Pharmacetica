3b4dd5c896c768e8278bfc6b050c1509384f0240
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2019-10-31 13:49:18 +0300

    Update bdd

--- Pharma.sql
+++ Pharma.sql
@@ -16,6 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Achat`
+--
+
+DROP TABLE IF EXISTS `Achat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Achat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Numero_commande` varchar(610) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `prix` int(11) NOT NULL,
+  `Client_id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Achat`
+--
+
+LOCK TABLES `Achat` WRITE;
+/*!40000 ALTER TABLE `Achat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Achat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Admin_admin`
 --
 
@@ -49,12 +77,14 @@ DROP TABLE IF EXISTS `Client`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Client` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cin` bigint(20) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(75) NOT NULL,
   `adresse` varchar(255) DEFAULT NULL,
   `telephone` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cin` (`cin`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +93,7 @@ CREATE TABLE `Client` (
 
 LOCK TABLES `Client` WRITE;
 /*!40000 ALTER TABLE `Client` DISABLE KEYS */;
+INSERT INTO `Client` VALUES (4,12345678912345,'gaetan','gaetan','Anjoma','0325398496'),(6,78945612378945,'oladad','rrrr','aaaaaa','0325896574');
 /*!40000 ALTER TABLE `Client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,14 +106,15 @@ DROP TABLE IF EXISTS `Commande`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Commande` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `produit_id` int(11) NOT NULL,
+  `Numero` varchar(610) NOT NULL DEFAULT current_timestamp(),
+  `produit_name` varchar(255) NOT NULL,
   `nbr_produit` int(50) NOT NULL,
-  `client_id` int(11) NOT NULL,
-  `date_commande` datetime NOT NULL,
-  `date_livraison` datetime DEFAULT NULL,
+  `client_id` bigint(20) NOT NULL,
+  `date_commande` datetime NOT NULL DEFAULT current_timestamp(),
   `est_livree` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Numero` (`Numero`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,8 +123,24 @@ CREATE TABLE `Commande` (
 
 LOCK TABLES `Commande` WRITE;
 /*!40000 ALTER TABLE `Commande` DISABLE KEYS */;
+INSERT INTO `Commande` VALUES (81,'current_timestamp()','Paracetamol',2,123456789,'2019-09-18 06:58:57',0);
 /*!40000 ALTER TABLE `Commande` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`gaetan`@`localhost`*/ /*!50003 TRIGGER `AssignCommande` AFTER INSERT ON `Commande` FOR EACH ROW UPDATE produit SET nombre_commande = new.nbr_produit, nombre_disponible = nombre - new.nbr_produit WHERE nom = new.produit_name */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `produit`
@@ -102,7 +150,7 @@ DROP TABLE IF EXISTS `produit`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produit` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   `prix_unit` float NOT NULL,
   `nombre_disponible` int(11) DEFAULT NULL,
@@ -111,7 +159,7 @@ CREATE TABLE `produit` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +168,6 @@ CREATE TABLE `produit` (
 
 LOCK TABLES `produit` WRITE;
 /*!40000 ALTER TABLE `produit` DISABLE KEYS */;
-INSERT INTO `produit` VALUES (1,'Nivaquinem',255,NULL,NULL,52),(2,'mmm',4444,NULL,NULL,585),(10,'cvb',4444,NULL,NULL,550000),(243,'fefef',454,NULL,NULL,8451),(5475,'french',44,NULL,NULL,55),(10000,'alcool',4545,NULL,NULL,5855555),(20136,'kali linux',2588,NULL,NULL,585),(20745,'Jamala',55,NULL,NULL,45878),(45455,'Nivaquine',4545,NULL,NULL,55),(45874,'rfrffrfrfv',4444,NULL,NULL,55),(56665,'gfhf',4444,NULL,NULL,42),(201201,'huindddd',4444,NULL,NULL,55),(256899,'Ataov matotra',55,NULL,NULL,5454),(258269,'moly',9,NULL,NULL,555558),(363939,'gollolog',55,NULL,NULL,585),(696555,'huikokt',4444000,NULL,NULL,5454),(1478965,'ffgffaaaa',55,NULL,NULL,55),(5525265,'hyhy',4444,NULL,NULL,55),(5858585,'killall',98989,NULL,NULL,5445),(23214587,'nitrogene',4545,NULL,NULL,5454),(25252852,'nolopoiuy',4545,NULL,NULL,5454),(44557855,'grgrgr',4444,NULL,NULL,55),(59595959,'poltic',52554700,NULL,NULL,58546953),(98564799,'sirop',98989,NULL,NULL,585),(454555555,'Nivaquinef',4444,NULL,NULL,55),(454557888,'Nivaquineygh',4444,NULL,NULL,5454);
 /*!40000 ALTER TABLE `produit` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-17  6:54:08
+-- Dump completed on 2019-09-18  7:05:48
